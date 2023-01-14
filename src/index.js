da55d96a92868dcd6d9a1c1e31caf78376742990
szbai <szabolcs.nagy.hu@gmail.com>
2023-01-14 11:47:06 +0100

    removed node_modules from remote

--- src/index.js
+++ src/index.js
@@ -7,9 +7,22 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+[];
 app.use(userRouter);
 app.use(taskRouter);
 
 app.listen(port, () => {
 	console.log("server is up on port " + port);
 });
+
+const bcrypt = require("bcrypt");
+
+const myfunc = async () => {
+	const password = "joska123";
+	const hashedPassword = await bcrypt.hash(password, 80);
+
+	console.log(password);
+	console.log(hashedPassword);
+};
+
+myfunc();
