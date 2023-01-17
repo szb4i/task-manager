8ecd222ee560d3a907700cf353cf4fe913c9968a
szbai <szabolcs.nagy.hu@gmail.com>
2023-01-17 14:28:18 +0100

    add timestamps to schemas

--- src/models/user.js
+++ src/models/user.js
@@ -4,53 +4,58 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const Task = require("./task");
 
-const userSchema = new mongoose.Schema({
-	name: {
-		type: String,
-		required: true,
-		trim: true,
-	},
-	email: {
-		type: String,
-		unique: true,
-		required: true,
-		trim: true,
-		lowercase: true,
-		validate(value) {
-			if (!validator.isEmail(value)) {
-				throw new Error("email is invalid");
-			}
+const userSchema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+			trim: true,
 		},
-	},
-	age: {
-		type: Number,
-		validate(value) {
-			if (value < 0) {
-				throw new Error("age must be a positive number");
-			}
-		},
-		default: 0,
-	},
-	password: {
-		type: String,
-		required: true,
-		trim: true,
-		minlength: 7,
-		validate(value) {
-			if (value.toLowerCase().includes("password")) {
-				throw new Error("password is not safe");
-			}
-		},
-	},
-	tokens: [
-		{
-			token: {
-				type: String,
-				required: true,
+		email: {
+			type: String,
+			unique: true,
+			required: true,
+			trim: true,
+			lowercase: true,
+			validate(value) {
+				if (!validator.isEmail(value)) {
+					throw new Error("email is invalid");
+				}
 			},
 		},
-	],
-});
+		age: {
+			type: Number,
+			validate(value) {
+				if (value < 0) {
+					throw new Error("age must be a positive number");
+				}
+			},
+			default: 0,
+		},
+		password: {
+			type: String,
+			required: true,
+			trim: true,
+			minlength: 7,
+			validate(value) {
+				if (value.toLowerCase().includes("password")) {
+					throw new Error("password is not safe");
+				}
+			},
+		},
+		tokens: [
+			{
+				token: {
+					type: String,
+					required: true,
+				},
+			},
+		],
+	},
+	{
+		timestamps: true,
+	}
+);
 
 userSchema.virtual("tasks", {
 	ref: "Task",
