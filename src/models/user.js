c1c000b9cc17fb1a6fdb8b0c714673e367d7424b
szbai <szabolcs.nagy.hu@gmail.com>
2023-01-24 15:11:46 +0100

    env variables

--- src/models/user.js
+++ src/models/user.js
@@ -67,7 +67,7 @@ userSchema.virtual("tasks", {
 });
 
 userSchema.methods.generateAuthToken = async function () {
-	const token = jwt.sign({ _id: this._id.toString() }, "thisismynewcourse");
+	const token = jwt.sign({ _id: this._id.toString() }, process.env.JWT_SECRET);
 	this.tokens = this.tokens.concat({ token });
 	await this.save();
 	return token;
